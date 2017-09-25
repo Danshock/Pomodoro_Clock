a5a5847f2a2d3c7aa09c229e1bcbcebb1d199cd2
Danislav <danito1233@gmail.com>
2017-09-25 11:54:03 +0100

    Added Clock&Break Function

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,67 @@ $(document).ready(function() {
   var buzzer = $("#buzzer")[0];
   var count = parseInt($("#num").html());
   var breakTime = parseInt($("#breakNum").html());
+ 
   //console.log(count);
   $("#reset").hide();	
 
-$("#start").click()
+//Counter
+  $("#start").click(function(){
+  var counter = setInterval(timer, 1000);
+  
+  
+
+  function timer(){
+  	//Hide variables
+  	$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2").hide();
+  	$("#timeType").show();
+  	$("#timeType").html("Session Time: ");
+
+  	count -= 1;
+  	if(count === 0){
+  	  buzzer.play();	
+  	  clearInterval(counter);
+  	  var startBreak = setInterval(breakTimer, 1000);
+  	  $("#num").hide();	
+  	}
+    $("#num").html(count);
 
 
+
+  	  function breakTimer(){
+    $("#timeType").html("Break Time: ");
+    $("#breakNum").show();
+
+
+    $("#timeType").show();
+    breakTime -= 1;
+    if(breakTime===0){
+    	clearInterval(startBreak);
+    	buzzer.play();
+    	$("#reset").show();
+    	$("#breakNum, #timeType").hide();	
+    }	
+    $("#breakNum").html(breakTime);
+  }
+} 
+
+  
+});
+
+
+  $("#reset").click(function(){
+  	count = 25;
+  	breakTime = 25;
+  	$("#num").html(count);
+  	$("#breakNum").html(breakTime);
+  	$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #num, #title1, #title2").show();
+
+  	$("#reset, #timeType").hide();
+
+  });
+
+
+//Clock Time
 $("#minus5Clock").click(function() {
   if(count > 5){	
   count -= 5;
@@ -21,7 +76,7 @@ $("#add5Clock").click(function() {
   $("#num").html(count);
   console.log(count);	  
 });
-
+//Break Time
 $("#minus5Break").click(function() {
   if(breakTime > 5){	
   breakTime -= 5;
